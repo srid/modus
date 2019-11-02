87f24969f1426dec1977b4f8fdefe04439063d6b
Sridhar Ratnakumar <srid@srid.ca>
2019-11-02 19:52:40 -0400

    Show raw data in frontend

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -1,29 +1,43 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 module Frontend where
 
+import Data.Functor.Identity
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
+
 import Obelisk.Configs
 import Obelisk.Frontend
 import Obelisk.Route
 import Reflex.Dom.Core
 
-import Common.Api
+import qualified Common.Plugin.TT as TT
 import Common.Route
-import Obelisk.Generated.Static
 
 frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
-  { _frontend_head = el "title" $ text "Obelisk Minimal Example"
+  { _frontend_head = el "title" $ text "📔Modus Dev"
   , _frontend_body = do
-      text "Welcome to Obelisk!"
-      el "p" $ text $ T.pack commonStuff
-      elAttr "img" ("src" =: static @"obelisk.jpg") blank
+      el "h1" $ text "Modus dev"
       el "div" $ do
+        d <- getData
+        widgetHold_ (text "Loading...") $ ffor d $ \case
+          Nothing -> text "Error decoding data"
+          Just items -> text $ T.pack $ show items
         exampleConfig <- getConfig "common/example"
         case exampleConfig of
           Nothing -> text "No config file found in config/common/example"
           Just s -> text $ T.decodeUtf8 s
   }
+
+getData
+  :: (MonadHold t m, PostBuild t m, Prerender js t m)
+  => m (Event t (Maybe [TT.Item]))
+getData = fmap switchDyn $ prerender (pure never) $ do
+  pb <- getPostBuild
+  getAndDecode $ renderBackendRoute enc (BackendRoute_GetData :/ ()) <$ pb
+  where
+    Right (enc :: Encoder Identity Identity (R (FullRoute BackendRoute FrontendRoute)) PageName) = checkEncoder fullRouteEncoder
