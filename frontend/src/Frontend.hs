62acd22fab45592778973f9d916eddb93c4db7b9
Sridhar Ratnakumar <srid@srid.ca>
2019-11-02 20:00:02 -0400

    Add semantic ui

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -11,6 +11,7 @@ import qualified Data.Text.Encoding as T
 
 import Obelisk.Configs
 import Obelisk.Frontend
+import Obelisk.Generated.Static
 import Obelisk.Route
 import Reflex.Dom.Core
 
@@ -19,14 +20,19 @@ import Common.Route
 
 frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
-  { _frontend_head = el "title" $ text "📔Modus Dev"
-  , _frontend_body = do
-      el "h1" $ text "Modus dev"
-      el "div" $ do
+  { _frontend_head = do
+      elAttr "base" ("href" =: "/") blank
+      elAttr "meta" ("name" =: "viewport" <> "content" =: "width=device-width, initial-scale=1") blank
+      el "title" $ text "📔Modus Dev"
+      elAttr "link" ("rel" =: "stylesheet" <> "type" =: "text/css" <> "href" =: static @"semantic.min.css") blank
+  , _frontend_body = divClass "ui container" $ do
+      elClass "h1" "ui header" $ text "Modus dev"
+      divClass "ui segment" $ do
         d <- getData
         widgetHold_ (text "Loading...") $ ffor d $ \case
           Nothing -> text "Error decoding data"
           Just items -> text $ T.pack $ show items
+      divClass "ui segment" $ do
         exampleConfig <- getConfig "common/example"
         case exampleConfig of
           Nothing -> text "No config file found in config/common/example"
